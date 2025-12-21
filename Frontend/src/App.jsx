c21dd7cf912aee604e45fa095032d73f80a40433
GAURAV DUBEY <nikhlidubey200789@gmail.com>
2025-12-21 10:58:05 +0530

    frontend setup done

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React,{ useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className='bg-green-400 p-4'>hello Issuely</h1>
+    </>
+  )
+}
+
+export default App
